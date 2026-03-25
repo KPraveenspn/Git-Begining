aa3e15def0e583ac6e1ed60cea7fce899953bba1
Praveen Kumar <kpraveenspn@gmail.com>
2026-03-25 19:11:38 +0530

    Update the UpdateDropDown class

--- src/UpdateDropdown.java
+++ src/UpdateDropdown.java
@@ -51,7 +51,7 @@ public class UpdateDropdown {
 		driver.findElement(By.id("divpaxinfo")).click();
 		    Thread.sleep(2000L);
 		    
-	/*int i=1;
+	/* int i=1;
 	while(i<5)
 	{
 		driver.findElement(By.id("hrefIncAdt")).click();
