ee8477a9fd95e4bf3a843ef64493683c086fc99c
Praveen Kumar <kpraveenspn@gmail.com>
2026-04-12 14:41:02 +0530

    Update the WindowHandles class

--- src/windowHandles.java
+++ src/windowHandles.java
@@ -18,7 +18,7 @@ public class windowHandles {
 		driver.findElement(By.xpath("//a[contains(text(),'Free Access to')]")).click();
 		Set<String> windows = driver.getWindowHandles();     //[ ParentId, ChildId ]
 		
-		Iterator<String> it = windows.iterator();
+		Iterator<String> it = windows.iterator();    //Iterator class
 		String parentID = it.next();              // Parent Id
 		String childID = it.next();              // Child Id
 		
