df8d3b5eeb14b2e82837d0270ab7968428c9ab61
Praveen Kumar <kpraveenspn@gmail.com>
2026-04-14 19:28:42 +0530

    Update the WindowActivities class

--- src/WindowActivities.java
+++ src/WindowActivities.java
@@ -9,7 +9,7 @@ public class WindowActivities {
 		//System.setProperty("webdriver.chrome.driver", "/Users/praveenkumar/Documents/chromedriver-mac-arm64/chromedriver");
 		
         WebDriver driver = new ChromeDriver();    //Initializing the WebDriver
-        driver.manage().window().maximize();   //To maximize the window6
+        driver.manage().window().maximize();     //To maximize the window6
         
         driver.get("http://google.com");    //launching google chrome
         
