f0cc8b89418691f037a32de307054b64be7513bf
Praveen Kumar <kpraveenspn@gmail.com>
2026-05-09 14:27:37 +0530

    New Update

--- src/windowHandles.java
+++ src/windowHandles.java
@@ -19,7 +19,6 @@ public class windowHandles {
 		
 		Set<String> windows = driver.getWindowHandles();      //[ ParentId, ChildId ]
 		
-		
 		Iterator<String> it = windows.iterator();      //Iterator class
 		String parentID = it.next();                  // Parent Id
 		String childID = it.next();                  // Child Id
