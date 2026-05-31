2ef2864fd6d81bdf675fa785af5c71b41530a4b0
Praveen Kumar <kpraveenspn@gmail.com>
2026-05-31 16:09:20 +0530

    New Update

--- src/Scope.java
+++ src/Scope.java
@@ -10,7 +10,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Scope {
 
 	public static void main(String[] args) throws InterruptedException {
-		// TODO Auto-generated method stub
 
         //1. Give me the count of links on the page
 		
