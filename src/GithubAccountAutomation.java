7703d0ba1ee085dfced33ef8b3a360798c4b2ff7
Praveen Kumar <kpraveenspn@gmail.com>
2026-07-18 20:45:11 +0530

    GithubAutomation

---
+++ src/GithubAccountAutomation.java
@@ -0,0 +1,53 @@
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class GithubAccountAutomation {
+
+	public static void main(String[] args) {
+		
+
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--start-maximized");
+		
+		WebDriver wd = new ChromeDriver(options);
+		
+		wd.get("https://github.com/signup");
+		
+		WebDriverWait wait = new WebDriverWait(wd, Duration.ofSeconds(10));
+		
+		By emailTextBoxLocator = By.id("email");
+		WebElement emailTextBox = wait.until(ExpectedConditions.visibilityOfElementLocated(emailTextBoxLocator));
+		emailTextBox.sendKeys("praveen1234@gmail.com");
+		
+		By passwordTextBoxLocator = By.id("password");
+		WebElement passwordTextBox = wait.until(ExpectedConditions.visibilityOfElementLocated(passwordTextBoxLocator));
+		passwordTextBox.sendKeys("praveen1234@");
+		
+		By userNameTextBoxLocator = By.id("login");
+		WebElement userNameTextBox = wait.until(ExpectedConditions.visibilityOfElementLocated(userNameTextBoxLocator));
+		userNameTextBox.sendKeys("praveen123456");
+		
+		By countryDropDownButtonLocator = By.xpath("//button[contains(@id,'select-panel')]");
+		WebElement countryDropDownButton = wait.until(ExpectedConditions.visibilityOfElementLocated(countryDropDownButtonLocator));
+		countryDropDownButton.click();
+		
+		By countrySearchBoxLocator = By.xpath("//input[contains(@id,'select-panel') and @type='search']");
+		WebElement countrySearchBox = wait.until(ExpectedConditions.visibilityOfElementLocated(countrySearchBoxLocator));
+		countrySearchBox.sendKeys("Albania");
+		
+		By countryButtonLocator = By.xpath("//span[contains(text(),'Albania')]/..");
+		WebElement countryButton= wait.until(ExpectedConditions.visibilityOfElementLocated(countryButtonLocator));
+		countryButton.click();
+		
+		
+
+	}
+
+}
--- src/Scope.java
+++ src/Scope.java
@@ -45,11 +45,11 @@ public class Scope {
 		}   //Open all the tabs
 		
 			
-		Set<String> abc=driver.getWindowHandles(); //4
+		 Set<String> abc=driver.getWindowHandles(); //4
 			Iterator<String> it=abc.iterator();
 			
 			
-			while(it.hasNext())    //It will iterate all the tabs to give the title
+			while(it.hasNext())   //It will iterate all the tabs to give the title
 			{
 				
 				driver.switchTo().window(it.next());
